ef9be8433c321baae51f2044f4bfca5e4604fbe6
Nofal <oomar.nofall@gmail.com>
2024-02-14 19:21:16 +0200

    modifying config to be able to design

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,22 @@
-const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
-    entry: {
-        bundle: path.resolve(__dirname, 'src/index.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name][contenthash].js',
-        clean: true,
-        assetModuleFilename: '[name][ext]',
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, 'dist'),
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-    },
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }
         ],
     },
     plugins: [
